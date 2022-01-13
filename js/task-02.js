80b7063f158204d372bc2fadaf66d8b482563a38
Iryna S <irynaradkovska@gmail.com>
2022-01-13 05:04:02 +0200

    Updates the 2nd task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const List = document.querySelector("#ingredients");
+
+const ingredientsList = ingredients.map(item => {
+  const ingredient = document.createElement("li");
+  ingredient.classList.add("item");
+  ingredient.textContent = item;
+  return ingredient;
+});
+
+List.append(...ingredientsList);
+console.log(ingredientsList);
