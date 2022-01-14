2f1bfeacbcd344e29b567c653ebe81fdc73659f2
Iryna S <irynaradkovska@gmail.com>
2022-01-14 07:52:04 +0200

    Updates the 9th task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector(`body`);
+const changeColorBtn = document.querySelector(`.change-color`);
+const color = document.querySelector(`.color`);
+
+changeColorBtn.addEventListener(`click`, (event) => {
+    event.preventDefault();
+    bodyEl.style.backgroundColor = getRandomHexColor();
+    color.textContent = getRandomHexColor();
+
+});
