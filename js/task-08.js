71fd10fe563152e8120afef022c01126c3ac0a07
Iryna S <irynaradkovska@gmail.com>
2022-01-14 07:31:14 +0200

    Updates the 8th task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    
+    if(email.value === "" || password.value === "") {
+        return alert("Fill in all the fields, please");
+    };
+
+    const enteredData = {
+        email: email.value,
+        password: password.value,
+    }
+
+    console.log(enteredData);
+    event.currentTarget.reset();
+});
